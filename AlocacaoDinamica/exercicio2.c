00c7aa76cfedc6c972b4a4c50200ad8003015d7d
dudustefanello <eduardostefanello3@gmail.com>
2016-03-09 21:33:56 -0300

    Update exercicio2.c
    alteração do prototipo da função que nao entendi o por que desse padrao

--- AlocacaoDinamica/exercicio2.c
+++ AlocacaoDinamica/exercicio2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* funcao(int *vetor1, int tamanho1, int *vetor2, int tamanho2){
+int* uniao(int *v1, int n1, int *v2, int n2){
 	int *vetor3;
 	int i, j;
 
-	vetor3 = (int *) malloc((tamanho1 + tamanho2) * sizeof(int));
+	vetor3 = (int *) malloc((n1 + n2) * sizeof(int));
 
-	for(i = 0; i < tamanho1; i++)
-		vetor3[i] = vetor1[i];
+	for(i = 0; i < n1; i++)
+		vetor3[i] = v1[i];
 
-	for(i; i < tamanho2 + tamanho1; i++)
-		vetor3[i] = vetor2[i - tamanho2];
+	for(i; i < n2 + n1; i++)
+		vetor3[i] = v2[i - n2];
 
 	return vetor3;
 }
@@ -19,7 +19,7 @@ int* funcao(int *vetor1, int tamanho1, int *vetor2, int tamanho2){
 int main(){
 	int vet1[3] = {12, 24, 36};
 	int vet2[3] = {14, 28, 42}, i;
-	int *vet3 = funcao(vet1, 3, vet2, 3);
+	int *vet3 = uniao(vet1, 3, vet2, 3);
 
 	for(i = 0; i < 3 + 3; i++)
 		printf("v3[%d] == %d\n", i, vet3[i]);
